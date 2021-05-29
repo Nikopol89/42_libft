91436c08981c20ca16434e7e5ae138ce84c1cdbd
Jacopo <jacopomartini89@gmail.com>
2021-05-29 19:12:33 +0200

    update to norm v3

---
+++ var/norm2_libft/ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmartini <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/20 15:26:53 by jmartini          #+#    #+#             */
+/*   Updated: 2021/01/20 15:26:55 by jmartini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void *ptr;
+
+	if (!size)
+	{
+		ptr = malloc(size);
+		return (ptr);
+	}
+	if (!(ptr = malloc(size * count)))
+		return (0);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
