1a2a1b75ecad4a7ffae3feb9e77b28e52780d4aa
Jacopo <jacopomartini89@gmail.com>
2021-05-29 22:54:27 +0200

    fix header norm

--- libft.h
+++ libft.h
@@ -17,7 +17,7 @@
 # include <unistd.h>
 
 typedef unsigned char	t_byte;
-typedef struct			s_list
+typedef struct s_list
 {
 	void				*content;
 	struct s_list		*next;
@@ -27,7 +27,7 @@ void					*ft_memset(void *b, int c, size_t len);
 void					ft_bzero(void *s, size_t n);
 void					*ft_memcpy(void *dst, const void *src, size_t n);
 void					*ft_memccpy(void *dst, const void *src,
-						int c, size_t n);
+							int c, size_t n);
 void					*ft_memmove(void *dst, const void *src, size_t len);
 void					*ft_memchr(const void *s, int c, size_t n);
 int						ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -37,7 +37,7 @@ size_t					ft_strlcat(char *dst, const char *src, size_t dstsize);
 char					*ft_strchr(const char *s, int c);
 char					*ft_strrchr(const char *s, int c);
 char					*ft_strnstr(const char *haystack,
-						const char *needle, size_t len);
+							const char *needle, size_t len);
 int						ft_strncmp(const char *s1, const char *s2, size_t n);
 int						ft_atoi(const char *str);
 int						ft_isalpha(int c);
@@ -50,13 +50,13 @@ int						ft_tolower(int c);
 void					*ft_calloc(size_t count, size_t size);
 char					*ft_strdup(const char *s1);
 char					*ft_substr(char const *s,
-						unsigned int start, size_t len);
+							unsigned int start, size_t len);
 char					*ft_strjoin(char const *s1, char const *s2);
 char					*ft_strtrim(char const *s1, char const *set);
 char					**ft_split(char const *s, char c);
 char					*ft_itoa(int n);
 char					*ft_strmapi(char const *s,
-						char (*f)(unsigned int, char));
+							char (*f)(unsigned int, char));
 void					ft_putchar_fd(char c, int fd);
 void					ft_putstr_fd(char *s, int fd);
 void					ft_putendl_fd(char *s, int fd);
